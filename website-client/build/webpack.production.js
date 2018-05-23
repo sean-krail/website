50a6abbe32094369159af74bddc0f763154dfa7a
Sean Krail <i@seankrail.com>
2018-05-22 22:49:22 -0700

    CI/CD Pipeline - TravisCI Integration (#5)
    * travis
    
    * move steps from deploy to script
    
    * set node to lts 8 and use yarn over npm
    
    * set node to lts carbon - 8
    
    * add missing then
    
    * travis-ci integration
    
    * debugging
    
    * try installing node and yarn from different source
    
    * fix node version
    
    * revert yarn repo change
    
    * debugging
    
    * remove debugging
    
    * default to unsetting the AWS_S3_BUCKET env var

--- website-client/build/webpack.production.js
+++ website-client/build/webpack.production.js
@@ -5,6 +5,6 @@ const commonConfig = require('./webpack.common.js');
 module.exports = webpackMerge(commonConfig, {
   output: {
     path: path.resolve(__dirname, "..", "dist"),
-    filename: "app.[hash].js"
+    filename: "app.js"
   }
 });

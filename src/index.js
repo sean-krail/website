be143a3961adb0fcbd2d53b894236bd90141d70d
Sean Krail <sean@Seans-MacBook-Pro.local>
2019-11-17 14:56:59 -0800

    first commit

---
+++ src/index.js
@@ -0,0 +1,38 @@
+let backgroundImages = [
+  {src: "static/Mount-Diablo-Bay-View.min.jpg", loaded: false},
+  {src: "static/Mission-Dolores-Park.min.jpg", loaded: false},
+  {src: "static/Alameda-South-Shore-Beach.min.jpg", loaded: false}
+];
+let backgroundImage = backgroundImages[0];
+let backgroundElement = document.getElementById("background");
+let numLoaded = 0;
+
+function setBackgroundImage(image) {
+  let url = "url(" + image.src + ")";
+  backgroundImage = image;
+  backgroundElement.style.backgroundImage = url;
+}
+
+function changeImage() {
+  let images = backgroundImages.filter((image) => image.loaded);
+  if (images.length > 1) {
+    let index = images.indexOf(backgroundImage);
+    index++;
+    if (index === images.length) index = 0;
+    setBackgroundImage(images[index]);
+  }
+}
+
+backgroundImages.forEach((image) => {
+  let element = document.createElement("img");
+  element.onload = () => {
+    image.loaded = true;
+    if (numLoaded === 0) {
+      setBackgroundImage(image);
+      setInterval(changeImage, 15000);
+    }
+    numLoaded++;
+    element.remove();
+  };
+  element.src = image.src;
+});

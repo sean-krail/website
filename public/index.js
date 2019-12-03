1e1d605f683c74b9561388685f02860a193bb498
Sean Krail <sean-krail@users.noreply.github.com>
2019-12-03 00:49:02 -0800

    fix error message and update border radius animation

--- public/index.js
+++ public/index.js
@@ -39,29 +39,73 @@ backgroundImages.forEach((image) => {
 const myface_container = document.getElementById("myface-container");
 const myface_border = document.getElementById("myface-border");
 const myface_image = document.getElementById("myface-image");
+
+let run_loop = true;
+let top_right = false;
+let bottom_right = true;
+let bottom_left = false;
+let top_left = false;
+
+function updateBorderRadius() {
+  let className = "";
+  className += top_right ? "top-right " : "";
+  className += bottom_right ? "bottom-right " : "";
+  className += bottom_left ? "bottom-left " : "";
+  className += top_left ? "top-left" : "";
+  myface_border.className = className
+}
+
+myface_container.addEventListener("mouseout", e => {
+  run_loop = true;
+});
+
 myface_container.addEventListener("mousemove", e => {
   const x0 = e.target === myface_container ? e.target.clientLeft : (e.target === myface_image ? e.target.parentElement.clientLeft : null);
   const y0 = e.target === myface_container ? e.target.clientTop : (e.target === myface_image ? e.target.parentElement.clientTop : null);
   const x1 = e.target === myface_container ? e.target.clientWidth + x0 : (e.target === myface_image ? e.target.parentElement.clientWidth + x0 : null);
   const y1 = e.target === myface_container ? e.target.clientHeight +y0 : (e.target === myface_image ? e.target.parentElement.clientHeight + y0 : null);
-  if (!x0 || !y0 || !x1 || !y1) {
+  if (x0 === null || y0 === null || x1 === null || y1 === null) {
     console.error("Whoops! There's a bug. Help me by reporting it at https://github.com/sean-krail/website/issues/new");
   }
+  run_loop = false;
   const xHalf = x0 + (x0 + x1) / 2;
   const yHalf = y0 + (y0 + y1) / 2;
   const x = e.layerX;
   const y = e.layerY;
+  top_right = false;
+  bottom_right = false;
+  bottom_left = false;
+  top_left = false;
   if (x < xHalf) {
     if (y < yHalf) {
-      myface_border.className = "top-left"
+      top_left = true;
     } else {
-      myface_border.className = "bottom-left"
+      bottom_left = true;
     }
   } else {
     if (y < yHalf) {
-      myface_border.className = "top-right"
+      top_right = true;
     } else {
-      myface_border.className = "bottom-right"
+      bottom_right = true;
     }
   }
+  updateBorderRadius();
 });
+
+setInterval(function() {
+  if (!run_loop) return;
+  if (top_right) {
+    top_right = false;
+    bottom_right = true;
+  } else if (bottom_right) {
+    bottom_right = false;
+    bottom_left = true;
+  } else if (bottom_left) {
+    bottom_left = false;
+    top_left = true;
+  } else {
+    top_left = false;
+    top_right = true;
+  }
+  updateBorderRadius();
+}, 3219);

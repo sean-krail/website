3afca9a180348714631f81ce8c641296b424e0da
Sean Krail <sean@krail.dev>
2023-05-23 18:44:43 -0400

    feat: use fontsource v5 and make myface non-selectable/draggable

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,21 +1,15 @@
 module.exports = {
   root: true,
-  parser: '@typescript-eslint/parser',
+  parser: "@typescript-eslint/parser",
   parserOptions: {
-    project: './tsconfig.json',
+    project: "./tsconfig.json",
   },
-  plugins: [
-    '@typescript-eslint',
-  ],
-  extends: [
-    'airbnb-typescript/base',
-    'prettier',
-    'plugin:import/recommended',
-  ],
+  plugins: ["@typescript-eslint"],
+  extends: ["airbnb-typescript/base", "prettier", "plugin:import/recommended"],
   settings: {
-    'import/resolver': {
+    "import/resolver": {
       parcel: {
-        rootDir: 'src',
+        rootDir: "src",
       },
     },
   },

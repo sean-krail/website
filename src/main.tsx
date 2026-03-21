6135c6f2f224ea83bfabfb167b7f0c5676e3f84c
Sean Krail <sean@krail.cloud>
2026-03-21 17:35:18 -0400

    feat: add bots and users counter

--- src/main.tsx
+++ src/main.tsx
@@ -1,17 +1,30 @@
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
+import { UAParser } from "ua-parser-js";
+import { Bots } from "ua-parser-js/extensions";
+import { isBot } from "ua-parser-js/bot-detection";
 import { App } from "~/App.tsx";
 import "~/index.css";
 
+(() => {
+  // See: https://github.com/faisalman/ua-parser-js/commit/bbcf24e
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  const url = isBot(UAParser(navigator.userAgent, Bots) as any)
+    ? import.meta.env.VITE_BOTS_COUNT_ENDPOINT
+    : import.meta.env.VITE_USERS_COUNT_ENDPOINT;
+  document.addEventListener("visibilitychange", () => {
+    if (document.visibilityState === "hidden") {
+      navigator.sendBeacon(url);
+    }
+  });
+})();
+
 const queryClient = new QueryClient();
-
 const rootElement = document.getElementById("root");
-
 if (!rootElement) {
   throw new Error("No #root element defined");
 }
-
 createRoot(rootElement).render(
   <StrictMode>
     <QueryClientProvider client={queryClient}>
